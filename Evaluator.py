df093376a90158ddf3fbb3c6261fc5489c36edc2
Cristian Turetta <cristian.turetta@me.com>
2019-05-17 18:54:08 +0200

    Fixed standard deviation

--- Evaluator.py
+++ Evaluator.py
@@ -7,22 +7,18 @@ import matplotlib.pyplot as plt
 def evaluate(dataset_name, dataset_path, output_path):
     dataframe = pd.DataFrame(pd.read_csv(dataset_path, sep=";"))
     elapsed_time = dataframe['time_difference'].sum()
-    total_data_income = (dataframe['total_volume'].sum())/(1**-6) #Mb
+    total_data_income = (dataframe['total_volume'].sum())/(1**6) #Mb
     traffic_mean_vol = total_data_income/elapsed_time
     traffic_mean = total_data_income/len(dataframe)
-    dataframe['total_volume'] /= (1**-6)
+    dataframe['total_volume'] /= (1**6)
 
     dataframe.insert(loc=0, column='id', value=range(1, len(dataframe) + 1))
 
     dataframe['margin_to_mean'] = dataframe['total_volume']
-    dataframe['margin_to_mean'] = dataframe['margin_to_mean'].apply(lambda x: x - traffic_mean)
+    dataframe['margin_to_mean'] = dataframe['margin_to_mean'].apply(lambda x: x - traffic_mean_vol)
 
-    dataframe['squared_margin_to_mean'] = dataframe['margin_to_mean'].apply(lambda x: (x - traffic_mean)**2)
+    dataframe['squared_margin_to_mean'] = dataframe['margin_to_mean'].apply(lambda x: (x - traffic_mean_vol)**2)
     sigma = math.sqrt(dataframe['squared_margin_to_mean'].sum()/(len(dataframe)-1))
-
-    dataframe['norm_squared_mtm'] = dataframe['squared_margin_to_mean']
-    dataframe['norm_squared_mtm'] -= dataframe['squared_margin_to_mean'].min()
-    dataframe['norm_squared_mtm'] /= dataframe['squared_margin_to_mean'].max()
 
     fig, ax = plt.subplots()
     dataframe.plot('id', 'ratio_vol_td', kind='scatter', ax=ax)
@@ -32,7 +28,6 @@ def evaluate(dataset_name, dataset_path, output_path):
     for i, point in islice(sub_dataframe.iterrows(), 0, 5):
         ax.text(point['id'], point['vol'], str(point['source']).split(",")[0].replace('(', ' '))
 
-    plt.axhline(traffic_mean, color='r')
     plt.axhline(traffic_mean_vol, color='g')
     plt.xlabel('IP ID')
     plt.ylabel('Mb/s')
@@ -40,16 +35,16 @@ def evaluate(dataset_name, dataset_path, output_path):
     plt.savefig(output_path + dataset_name + "-diff_from_mean_analysis.png", dpi=300)
 
     fig, ax = plt.subplots()
-    dataframe.plot('id', 'norm_squared_mtm', kind='scatter', ax=ax)
+    dataframe.plot('id', 'squared_margin_to_mean', kind='scatter', ax=ax)
 
-    sub_dataframe = pd.concat({'id': dataframe['id'], 'nsmtm': dataframe['norm_squared_mtm'],
+    sub_dataframe = pd.concat({'id': dataframe['id'], 'mtm': dataframe['squared_margin_to_mean'],
                                'source': dataframe['group']}, axis=1)
     for i, point in islice(sub_dataframe.iterrows(), 0, 5):
-        ax.text(point['id'], point['nsmtm'], str(point['source']).split(",")[0].replace('(', ' '))
+        ax.text(point['id'], point['mtm'], str(point['source']).split(",")[0].replace('(', ' '))
 
-    plt.axhline((sigma - dataframe['squared_margin_to_mean'].min())/dataframe['squared_margin_to_mean'].max(), color='g')
+    plt.axhline(sigma**2, color='r')
     plt.xlabel('IP ID')
-    plt.ylabel('Normalised squared margin to mean')
+    plt.ylabel('Squared margin to mean')
 
     plt.savefig(output_path + dataset_name + "-squared_margin_to_mean_analysis.png", dpi=300)
 
