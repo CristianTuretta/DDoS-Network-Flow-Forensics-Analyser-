670e9770436a31b2e69e4bbcbd70b134a5357618
Cristian Turetta <cristian.turetta@me.com>
2019-05-17 15:29:47 +0200

    Added squared margin to mean plot

--- Evaluator.py
+++ Evaluator.py
@@ -18,17 +18,35 @@ def evaluate(dataset_name, dataset_path, output_path):
     dataframe['squared_margin_to_mean'] = dataframe['margin_to_mean'].apply(lambda x: (x - traffic_mean)**2)
     sigma = math.sqrt(dataframe['squared_margin_to_mean'].sum()/(len(dataframe)-1))
 
+    dataframe['norm_squared_mtm'] -= dataframe['squared_margin_to_mean'].min()
+    dataframe['norm_squared_mtm'] /= dataframe['squared_margin_to_mean'].max()
+
     fig, ax = plt.subplots()
     dataframe.plot('id', 'ratio_vol_td', kind='scatter', ax=ax)
 
-    sub_dataframe = pd.concat({'id': dataframe['id'], 'vol': dataframe['ratio_vol_td'], 'source': dataframe['group']}, axis=1)
+    sub_dataframe = pd.concat({'id': dataframe['id'], 'vol': dataframe['ratio_vol_td'], 'source': dataframe['group']},
+                              axis=1)
     for i, point in islice(sub_dataframe.iterrows(), 0, 5):
         ax.text(point['id'], point['vol'], str(point['source']).split(",")[0].replace('(', ' '))
 
     plt.axhline(traffic_mean, color='r')
-    # plt.axhline(sigma, color='g')
     plt.xlabel('IP ID')
     plt.ylabel('MB/s')
 
-    plt.savefig(output_path + dataset_name + "-regression_analysis.png", dpi=300)
+    plt.savefig(output_path + dataset_name + "-diff_from_mean_analysis.png", dpi=300)
+
+    fig, ax = plt.subplots()
+    dataframe.plot('id', 'norm_squared_mtm', kind='scatter', ax=ax)
+
+    sub_dataframe = pd.concat({'id': dataframe['id'], 'nsmtm': dataframe['norm_squared_mtm'],
+                               'source': dataframe['group']}, axis=1)
+    for i, point in islice(sub_dataframe.iterrows(), 0, 5):
+        ax.text(point['id'], point['nsmtm'], str(point['source']).split(",")[0].replace('(', ' '))
+
+    plt.axhline(sigma, color='r')
+    plt.xlabel('IP ID')
+    plt.ylabel('Normalised squared margin to mean')
+
+    plt.savefig(output_path + dataset_name + "-squared_margin_to_mean_analysis.png", dpi=300)
+
     dataframe.to_csv(output_path + dataset_name + "-indexed", index=False)
