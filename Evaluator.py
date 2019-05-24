22c4d563658dd976bf96b82da126ad968adf0d3d
Andrea Perazzoli <andreaperazzoli95@gmail.com>
2019-05-24 16:42:17 +0200

    Changed MB to KB

--- Evaluator.py
+++ Evaluator.py
@@ -7,11 +7,11 @@ import matplotlib.pyplot as plt
 def evaluate(dataset_name, dataset_path, output_path):
     dataframe = pd.DataFrame(pd.read_csv(dataset_path, sep=";"))
     elapsed_time = dataframe['time_difference'].sum()
-    total_data_income = (dataframe['total_volume'].sum())/(10**6)  # MB
-    traffic_mean_vol = (total_data_income/elapsed_time)  # MB/s
-    traffic_mean = total_data_income/len(dataframe)  # MB per user
-    dataframe['total_volume'] = dataframe['total_volume'].apply(lambda x: x/(10**6))  # MB
-    dataframe['ratio_vol_td'] = dataframe['ratio_vol_td'].apply(lambda x: x/(10**6))  # MB/s
+    total_data_income = (dataframe['total_volume'].sum())/(10**3)  # KB
+    traffic_mean_vol = (total_data_income/elapsed_time)  # KB/s
+    traffic_mean = total_data_income/len(dataframe)  # KB per user
+    dataframe['total_volume'] = dataframe['total_volume'].apply(lambda x: x/(10**3))  # KB
+    dataframe['ratio_vol_td'] = dataframe['ratio_vol_td'].apply(lambda x: x/(10**3))  # KB/s
 
     dataframe.insert(loc=0, column='id', value=range(1, len(dataframe) + 1))
 
@@ -25,21 +25,21 @@ def evaluate(dataset_name, dataset_path, output_path):
     dataframe.plot('id', 'total_volume', kind='scatter', linewidth='0.5', ax=ax, label='Data exchanged')
     plt.axhline(traffic_mean, color='r', label='Mean')
     plt.xlabel('IP ID')
-    plt.ylabel('MB')
+    plt.ylabel('KB')
     plt.savefig(output_path + dataset_name + "-data_analysis.png", dpi=300)
 
     fig, ax = plt.subplots(figsize=(14, 6))
     dataframe.plot('id', 'ratio_vol_td', kind='scatter', linewidth='0.5', ax=ax, label='Volume per second')
     plt.axhline(traffic_mean_vol, color='g', label='Mean')
     plt.xlabel('IP ID')
-    plt.ylabel('MB/s')
+    plt.ylabel('KB/s')
     plt.savefig(output_path + dataset_name + "-volume_analysis.png", dpi=300)
 
     fig, ax = plt.subplots(figsize=(14, 6))
     dataframe.plot('id', 'squared_margin_to_mean', kind='scatter', linewidth='0.5', ax=ax, label='Squared margin')
     plt.axhline(sigma**2, color='r', label='Sigma')
     plt.xlabel('IP ID')
-    plt.ylabel('Squared Margin from mean (MB/s)')
+    plt.ylabel('Squared Margin from mean (KB/s)')
     plt.savefig(output_path + dataset_name + "-squared_margin_analysis.png", dpi=300)
 
     dataframe.to_csv(output_path + dataset_name + "-indexed", index=False)
