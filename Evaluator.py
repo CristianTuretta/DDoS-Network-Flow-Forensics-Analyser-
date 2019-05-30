af2194803ad0a140b55bacd36aa301376d08296c
Cristian Turetta <cristian.turetta@me.com>
2019-05-30 16:00:01 +0200

    Fixed mathematical model and plot enhancement

--- Evaluator.py
+++ Evaluator.py
@@ -15,31 +15,36 @@ def evaluate(dataset_name, dataset_path, output_path):
 
     dataframe.insert(loc=0, column='id', value=range(1, len(dataframe) + 1))
 
+    # Sigma over exchanged data
     dataframe['margin_to_mean'] = dataframe['total_volume']
-    dataframe['margin_to_mean'] = dataframe['margin_to_mean'].apply(lambda x: (x - traffic_mean_vol))
+    dataframe['margin_to_mean'] = dataframe['margin_to_mean'].apply(lambda x: (x - traffic_mean))
+    dataframe['squared_margin_to_mean'] = dataframe['margin_to_mean'].apply(lambda x: (x - traffic_mean)**2)
+    sigma_data = math.sqrt(dataframe['squared_margin_to_mean'].sum()/(len(dataframe)-1))
 
-    dataframe['squared_margin_to_mean'] = dataframe['margin_to_mean'].apply(lambda x: (x - traffic_mean_vol)**2)
-    sigma = math.sqrt(dataframe['squared_margin_to_mean'].sum()/(len(dataframe)-1))
+    # Sigma over traffic volume
+    dataframe['margin_to_mean_vol'] = dataframe['ratio_vol_td']
+    dataframe['margin_to_mean_vol'] = dataframe['ratio_vol_td'].apply(lambda x: (x - traffic_mean_vol))
+    dataframe['squared_margin_to_mean_vol'] = dataframe['margin_to_mean_vol'].apply(lambda x: (x - traffic_mean_vol) ** 2)
+    sigma_vol = math.sqrt(dataframe['squared_margin_to_mean_vol'].sum() / (len(dataframe) - 1))
 
     fig, ax = plt.subplots(figsize=(14, 6))
     dataframe.plot('id', 'total_volume', kind='scatter', linewidth='0.5', ax=ax, label='Data exchanged')
     plt.axhline(traffic_mean, color='r', label='Mean')
+    plt.axhline(sigma_data, color='g', label='Sigma')
     plt.xlabel('IP ID')
     plt.ylabel('KB')
+    plt.legend()
+    plt.title("Data Analysis")
     plt.savefig(output_path + dataset_name + "-data_analysis.png", dpi=300)
 
     fig, ax = plt.subplots(figsize=(14, 6))
     dataframe.plot('id', 'ratio_vol_td', kind='scatter', linewidth='0.5', ax=ax, label='Volume per second')
-    plt.axhline(traffic_mean_vol, color='g', label='Mean')
+    plt.axhline(traffic_mean_vol, color='r', label='Mean')
+    plt.axhline(sigma_vol, color='g', label='Sigma')
     plt.xlabel('IP ID')
     plt.ylabel('KB/s')
+    plt.legend()
+    plt.title("Traffic Volume Analysis")
     plt.savefig(output_path + dataset_name + "-volume_analysis.png", dpi=300)
-
-    fig, ax = plt.subplots(figsize=(14, 6))
-    dataframe.plot('id', 'squared_margin_to_mean', kind='scatter', linewidth='0.5', ax=ax, label='Squared margin')
-    plt.axhline(sigma**2, color='r', label='Sigma')
-    plt.xlabel('IP ID')
-    plt.ylabel('Squared Margin from mean (KB/s)')
-    plt.savefig(output_path + dataset_name + "-squared_margin_analysis.png", dpi=300)
 
     dataframe.to_csv(output_path + dataset_name + "-indexed", index=False)
